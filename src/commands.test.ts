d9596939e03c880f33668d4abcbe4ad4d24b88f7
Simon Accascina <simon@accascina.me>
2024-04-22 14:58:59 +0200

    selection symbols extraction

--- src/commands.test.ts
+++ src/commands.test.ts
@@ -3,26 +3,89 @@ import * as vsj from "jest-mock-vscode"
 import * as Cmd from "./commands"
 import * as ParserLib from "./parser"
 
+const re = /<IS>|<IE>|<FS>|<FE>/g
+const extractSymbols = (text: string) => {
+  const positions = Array.from(text.matchAll(re))
+  if (positions.length !== 4) {
+    throw new Error("Must have exactly 4 <> symbols")
+  }
+
+  const symbols: { [symbol: string]: number } = {}
+
+  symbols[positions[0][0]] = positions[0].index
+  symbols[positions[1][0]] = positions[1].index - 4
+  symbols[positions[2][0]] = positions[2].index - 4 - 4
+  symbols[positions[3][0]] = positions[3].index - 4 - 4 - 4
+
+  if (!symbols["<IS>"]) {
+    throw new Error("Missing symbol <IS>")
+  }
+
+  if (!symbols["<IE>"]) {
+    throw new Error("Missing symbol <IE>")
+  }
+
+  if (!symbols["<FS>"]) {
+    throw new Error("Missing symbol <FS>")
+  }
+
+  if (!symbols["<FE>"]) {
+    throw new Error("Missing symbol <FE>")
+  }
+
+  return symbols
+}
+
+const text2VScodeObjs = (
+  languageID: string,
+  text: string,
+): { doc: vscode.TextDocument; initialSel: vscode.Selection; finalSel: vscode.Selection } => {
+  const filteredText = text.replace(re, "")
+  const doc = vsj.createTextDocument(
+    vscode.Uri.parse("untitled:Untitled-1"),
+    filteredText,
+    languageID,
+  )
+
+  const symbols = extractSymbols(text)
+
+  const initialSel = new vscode.Selection(
+    doc.positionAt(symbols["<IS>"]),
+    doc.positionAt(symbols["<IE>"]),
+  )
+
+  const finalSel = new vscode.Selection(
+    doc.positionAt(symbols["<FS>"]),
+    doc.positionAt(symbols["<FE>"]),
+  )
+
+  return { doc, initialSel, finalSel }
+}
+
 describe("workspace", () => {
   test("SelectTopLevel", async () => {
     const parser = await ParserLib.initParser()
 
-    const doc = vsj.createTextDocument(
-      vscode.Uri.parse("untitled:Untitled-1"),
-      `
-const a = () => {
-
-}
-`,
+    const { doc, initialSel, finalSel } = text2VScodeObjs(
       "typescript",
+      `
+a = 123
+<FS>const<IS><IE> a = () => {
+    
+}<FE>
+    `,
     )
-    const initialSel = new vscode.Selection(new vscode.Position(2, 2), new vscode.Position(2, 2))
-    const finalSel = new vscode.Selection(new vscode.Position(1, 0), new vscode.Position(3, 1))
+
     await ParserLib.loadLanguage(".", doc.languageId)
     if (!ParserLib.setParserLanguage(parser, doc.languageId)) {
       throw new Error("Could not set parser language")
     }
     const res = Cmd.SelectTopLevel(doc, initialSel, parser.parse(doc.getText()))
-    expect(res?.isEqual(finalSel)).toBeTruthy()
+    const isEq = res?.isEqual(finalSel)
+
+    if (!isEq) {
+      console.error("Res", res)
+    }
+    expect(isEq).toBeTruthy()
   })
 })
