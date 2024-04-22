234ebeb808a1b40cf0051834d46e0e5311fb9bd8
Simon Accascina <simon@accascina.me>
2024-04-22 14:24:15 +0200

    first working unit test

--- src/commands.test.ts
+++ src/commands.test.ts
@@ -1,11 +1,11 @@
 import * as vscode from "vscode"
 import * as vsj from "jest-mock-vscode"
 import * as Cmd from "./commands"
-import { initParser, setParserLanguage } from "./parser"
+import * as ParserLib from "./parser"
 
 describe("workspace", () => {
   test("SelectTopLevel", async () => {
-    const parser = await initParser()
+    const parser = await ParserLib.initParser()
 
     const doc = vsj.createTextDocument(
       vscode.Uri.parse("untitled:Untitled-1"),
@@ -14,11 +14,15 @@ const a = () => {
 
 }
 `,
+      "typescript",
     )
-    const sel = new vscode.Selection(new vscode.Position(1, 1), new vscode.Position(1, 1))
-    if (!setParserLanguage(parser, "typescript")) {
+    const initialSel = new vscode.Selection(new vscode.Position(2, 2), new vscode.Position(2, 2))
+    const finalSel = new vscode.Selection(new vscode.Position(1, 0), new vscode.Position(3, 1))
+    await ParserLib.loadLanguage(".", doc.languageId)
+    if (!ParserLib.setParserLanguage(parser, doc.languageId)) {
       throw new Error("Could not set parser language")
     }
-    Cmd.SelectTopLevel(doc, sel, parser.parse(doc.getText()))
+    const res = Cmd.SelectTopLevel(doc, initialSel, parser.parse(doc.getText()))
+    expect(res?.isEqual(finalSel)).toBeTruthy()
   })
 })
