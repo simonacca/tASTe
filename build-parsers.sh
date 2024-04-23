fe5d090f2ee35882b988970812f0848c01862789
Simon Accascina <simon@accascina.me>
2024-04-23 17:25:02 +0200

    one more try

--- build-parsers.sh
+++ build-parsers.sh
@@ -26,63 +26,59 @@ build_parser tree-sitter-c c
 build_parser tree-sitter-c-sharp c-sharp
 build_parser tree-sitter-commonlisp commonlisp
 build_parser tree-sitter-cpp cpp
-build_parser tree-sitter-capnp capnp
-build_parser tree-sitter-clojure clojure
-build_parser tree-sitter-cobol cobol
-build_parser tree-sitter-css css
-build_parser tree-sitter-cuda cuda
-build_parser tree-sitter-dart dart
-build_parser tree-sitter-dockerfile dockerfile
-build_parser tree-sitter-dot dot
-build_parser tree-sitter-elixir elixir
-build_parser tree-sitter-erlang erlang
-build_parser tree-sitter-fish fish
-build_parser tree-sitter-go go
-build_parser tree-sitter-gomod go-mod
-build_parser tree-sitter-graphql graphql
-build_parser tree-sitter-hacklang hack
-build_parser tree-sitter-haskell haskell
-build_parser tree-sitter-hcl hcl
-build_parser tree-sitter-html html
-build_parser tree-sitter-java java
-build_parser tree-sitter-javascript javascript
-build_parser tree-sitter-jq jq
-build_parser tree-sitter-json json
-build_parser tree-sitter-julia julia
-build_parser tree-sitter-kotlin kotlin
-# build_parser tree-sitter-latex latex # 2024-04 (expects parser.h)
-build_parser tree-sitter-lua lua
-build_parser '@tree-sitter-grammars/tree-sitter-markdown/tree-sitter-markdown' markdown
-build_parser tree-sitter-matlab matlab
-# build_parser tree-sitter-nginx nginx # 2024-04 (Cannot find module 'nan')
-build_parser tree-sitter-nix nix
-build_parser tree-sitter-objc objc
-# build_parser tree-sitter-ocaml/grammars/ocaml ocaml # 2024-04 (can't find scanner.h)
-# build_parser tree-sitter-perl perl # 2024-04 (can't find grammar.json)
-# build_parser tree-sitter-pascal pascal # 2024-04 (can't find binding.o)
-# build_parser tree-sitter-php/php php # 2024-04 (can't find scanner.h)
-build_parser tree-sitter-proto proto
-build_parser tree-sitter-python python
-build_parser tree-sitter-qmljs qmljs
-build_parser tree-sitter-r r
-build_parser tree-sitter-racket racket
-build_parser tree-sitter-rust rust
-build_parser tree-sitter-ruby ruby
-build_parser tree-sitter-scala scala
-build_parser tree-sitter-scheme scheme
-build_parser tree-sitter-scss scss
-build_parser tree-sitter-sql sql
-build_parser tree-sitter-swift swift
-build_parser tree-sitter-toml toml
-build_parser tree-sitter-typescript/typescript typescript
-build_parser tree-sitter-typescript/tsx typescriptreact
-# build_parser tree-sitter-xml xml # 2024-04 (can't find scanner.h)
+# build_parser tree-sitter-capnp capnp
+# build_parser tree-sitter-clojure clojure
+# build_parser tree-sitter-cobol cobol
+# build_parser tree-sitter-css css
+# build_parser tree-sitter-cuda cuda
+# build_parser tree-sitter-dart dart
+# build_parser tree-sitter-dockerfile dockerfile
+# build_parser tree-sitter-dot dot
+# build_parser tree-sitter-elixir elixir
+# build_parser tree-sitter-erlang erlang
+# build_parser tree-sitter-fish fish
+# build_parser tree-sitter-go go
+# build_parser tree-sitter-gomod go-mod
+# build_parser tree-sitter-graphql graphql
+# build_parser tree-sitter-hacklang hack
+# build_parser tree-sitter-haskell haskell
+# build_parser tree-sitter-hcl hcl
+# build_parser tree-sitter-html html
+# build_parser tree-sitter-java java
+# build_parser tree-sitter-javascript javascript
+# build_parser tree-sitter-jq jq
+# build_parser tree-sitter-json json
+# build_parser tree-sitter-julia julia
+# build_parser tree-sitter-kotlin kotlin
+# # build_parser tree-sitter-latex latex # 2024-04 (expects parser.h)
+# build_parser tree-sitter-lua lua
+# build_parser '@tree-sitter-grammars/tree-sitter-markdown/tree-sitter-markdown' markdown
+# build_parser tree-sitter-matlab matlab
+# # build_parser tree-sitter-nginx nginx # 2024-04 (Cannot find module 'nan')
+# build_parser tree-sitter-nix nix
+# build_parser tree-sitter-objc objc
+# # build_parser tree-sitter-ocaml/grammars/ocaml ocaml # 2024-04 (can't find scanner.h)
+# # build_parser tree-sitter-perl perl # 2024-04 (can't find grammar.json)
+# # build_parser tree-sitter-pascal pascal # 2024-04 (can't find binding.o)
+# # build_parser tree-sitter-php/php php # 2024-04 (can't find scanner.h)
+# build_parser tree-sitter-proto proto
+# build_parser tree-sitter-python python
+# build_parser tree-sitter-qmljs qmljs
+# build_parser tree-sitter-r r
+# build_parser tree-sitter-racket racket
+# build_parser tree-sitter-rust rust
+# build_parser tree-sitter-ruby ruby
+# build_parser tree-sitter-scala scala
+# build_parser tree-sitter-scheme scheme
+# build_parser tree-sitter-scss scss
+# build_parser tree-sitter-sql sql
+# build_parser tree-sitter-swift swift
+# build_parser tree-sitter-toml toml
+# build_parser tree-sitter-typescript/typescript typescript
+# build_parser tree-sitter-typescript/tsx typescriptreact
+# # build_parser tree-sitter-xml xml # 2024-04 (can't find scanner.h)
 build_parser tree-sitter-zig zig
 # build_parser tree-sitter-vue vue # 2024-04 (no member AcessorSignature in namespace v8)
-
-if [ -n "${BUILDERS_COUNT:-}" ]; then 
-    parallel --wait --id builder
-fi
 
 
 echo "Done building parsers"
